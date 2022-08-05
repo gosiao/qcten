b8642bcbb3a4d7d133843d1eb8343236f2fbe062
gosia olejniczak <gosia.olejniczak@gmail.com>
2022-08-05 11:52:44 +0200

    Initial commit after CMS Cookiecutter creation, version 1.6

---
+++ qcten/__init__.py
@@ -0,0 +1,11 @@
+"""Write me"""
+
+# Add imports here
+from .main import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
