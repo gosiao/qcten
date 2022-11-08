c4d458c5c68ddab941e15a6c8ead59575eef6498
gosia olejniczak <gosia.olejniczak@gmail.com>
2022-11-08 15:09:26 +0100

    trying to fix GA; start with simplifying setuptools config

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ import versioneer
 
 short_description = "Write me".split("\n")[0]
 
-# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
@@ -18,9 +17,7 @@ try:
 except:
     long_description = None
 
-
 setup(
-    # Self-descriptive entries which should always be present
     name='qcten',
     author='Gosia Olejniczak',
     author_email='gosia.olejniczak@gmail.com',
@@ -30,30 +27,6 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     license='MIT',
-
-    # Which Python importable modules should be included when your package is installed
-    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
-    # subpackage(s) from being added, if needed
     packages=find_packages(where="qcten"),
-
-    # Optional include package data to ship with your package
-    # Customize MANIFEST.in if the general case does not suit your needs
-    # Comment out this line to prevent the files from being packaged with your software
-    include_package_data=True,
-
-    # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
-
-    # Additional entries you may want simply uncomment the lines you want and fill in the data
-    # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
-    # platforms=['Linux',
-    #            'Mac OS-X',
-    #            'Unix',
-    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
-
-    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
-    # zip_safe=False,
-
 )
